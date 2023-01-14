4442f44a45477b0e4ff5e164744643e55c48ffa2
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-14 09:42:01 +0800

    Update login2.js

--- login2.js
+++ login2.js
@@ -6,8 +6,6 @@ if(username=="admin"&& password=="user")
 {
     alert("login succesfully");
     window.open("https://zoro.to/watch/farming-life-in-another-world-18284?ep=97013");
-    return false;
-
 }
 else
 {
