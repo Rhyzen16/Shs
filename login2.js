eba78a4bb65304e9eb180c935a87e251fa5657a2
Rhyzen16 <102942367+Rhyzen16@users.noreply.github.com>
2023-01-14 09:53:21 +0800

    Update login2.js

--- login2.js
+++ login2.js
@@ -2,7 +2,7 @@ function validate()
 {
 var username=document.getElementById("username").value;
 var password=document.getElementById("password").value;
-if(username=="admin"&& password=="user")
+if(username=="admin1@rhyzen16"&& password=="/remeo/sierra/papa/yankee/")
 {
     alert("login succesfully");
     window.open("https://zoro.to/watch/farming-life-in-another-world-18284?ep=97013");
